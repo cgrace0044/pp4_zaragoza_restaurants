83aabac2146ffa3a513620bffb606faf0463b744
cgrace0044 <cgrace0044@gmail.com>
2025-05-29 16:23:36 +0200

    create restaurant and comment model in model.py

--- letters_from_zaragoza/apps.py
+++ letters_from_zaragoza/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LettersFromZaragozaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'letters_from_zaragoza'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "letters_from_zaragoza"
--- letters_from_zaragoza/models.py
+++ letters_from_zaragoza/models.py
@@ -1,26 +1,64 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, "Save for later"), (1, "Publish now"))
+
 
 # Create your models here.
 class Restaurant(models.Model):
-    PRICE_RANGE_CHOICES = [
-        ('€', 'Budget'),
-        ('€€', 'Moderate'),
-        ('€€€', 'Expensive'),
-        ('€€€€', 'Luxury'),
-    ]
+    """Model for Restaurant"""
 
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="restaurant_review"
-)
+        User, on_delete=models.CASCADE, related_name="restaurant_review"
+    )
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=200, unique=True)
-    price_range = models.CharField(max_length=4, choices=PRICE_RANGE_CHOICES)
+    low_price_range = models.IntegerField(default=0)
+    high_price_range = models.IntegerField(default=10)
     description = models.TextField()
-    opening_hours = models.CharField(max_length=300)
+    opening_hours = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField("image", default="placeholder")
     status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='restaurant_like', blank=True)
+
+    def __str__(self):
+        return f"{self.name} | written by {self.author}"
+    
+    def number_of_likes(self):
+        return self.likes.count()
+
+    @property
+    def price_range(self) -> str:
+        if self.high_price_range <= 10:
+            return "Budget"
+        elif self.high_price_range <= 20:
+            return "Moderate"
+        elif self.high_price_range <= 30:
+            return "Expensive"
+        else:
+            return "Luxury"
+
+
+class Comment(models.Model):
+    """Model for Comment"""
+
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """To display the comments by created_on in ascending order"""
+
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author.get_full_name()}"
