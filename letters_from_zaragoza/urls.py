1fa25975f628a157535ba21d0e3716abc20bb2ea
cgrace0044 <cgrace0044@gmail.com>
2025-06-03 16:25:49 +0200

    added restaurant_detail view

--- letters_from_zaragoza/urls.py
+++ letters_from_zaragoza/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.Home.as_view(), name="home"),
     path('browse_restaurants/', views.RestaurantList.as_view(), 
          name="browse_restaurants"),
+    path('<slug:slug>/', views.restaurant_detail, name='restaurant_detail'),
 ]
--- letters_from_zaragoza/views.py
+++ letters_from_zaragoza/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Restaurant
 
@@ -19,3 +19,18 @@ class RestaurantList(generic.ListView):
     queryset = Restaurant.objects.filter(status=1)
     template_name = "browse_restaurants.html"
     paginate_by = 6
+
+
+def restaurant_detail(request, slug):
+    """
+    This view is used to display the full restaurant details.
+    """
+
+    queryset = Restaurant.objects.filter(status=1)
+    restaurant = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "restaurant_detail.html",
+        {"restaurant": restaurant},
+    )
