e1a2cd6c715635f5b404f7b39dcc20204bb4dd78
cgrace0044 <cgrace0044@gmail.com>
2025-06-09 16:12:59 +0200

    create edit and delete functionality for restaurant comments

--- letters_from_zaragoza/urls.py
+++ letters_from_zaragoza/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('browse_restaurants/', views.RestaurantList.as_view(), 
          name="browse_restaurants"),
     path('<slug:slug>/', views.restaurant_detail, name='restaurant_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+     path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
