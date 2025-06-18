e6da22ad362c5a221ec916b9808112afb45e9a52
cgrace0044 <cgrace0044@gmail.com>
2025-06-18 14:02:27 +0200

    improve commentary on views

--- letters_from_zaragoza/views.py
+++ letters_from_zaragoza/views.py
@@ -9,15 +9,15 @@ from .forms import CommentForm
 
 # Create your views here.
 class Home(generic.TemplateView):
-    """This view is used to display the home page"""
+    """View for rendering the homepage"""
 
     template_name = "index.html"
 
 
 class RestaurantList(generic.ListView):
     """
-    Display a list of approved reviews
-
+    Displays a paginated list of approved restaurants, ordered by creation
+    date.
     """
 
     model = Restaurant
@@ -28,7 +28,8 @@ class RestaurantList(generic.ListView):
 
 def restaurant_detail(request, slug):
     """
-    This view is used to display the full restaurant details.
+    Displays detailed view of a specific restaurant, along with its comments.
+    Handles new comment submissions.
     """
 
     queryset = Restaurant.objects.filter(status=1)
@@ -59,10 +60,14 @@ def restaurant_detail(request, slug):
     )
 
 
+@login_required
 def comment_edit(request, slug, comment_id):
+
     """
-    view to edit comments
+    Allows a logged-in user to edit their own comment on a restaurant.
+    The comment will be marked as unapproved after editing.
     """
+
     if request.method == "POST":
         queryset = Restaurant.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
@@ -71,7 +76,7 @@ def comment_edit(request, slug, comment_id):
 
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
-            comment.post = post
+            comment.restaurant = post
             comment.approved = False
             comment.save()
             messages.add_message(request, messages.SUCCESS, "Comment Updated!")
@@ -81,10 +86,13 @@ def comment_edit(request, slug, comment_id):
     return HttpResponseRedirect(reverse("restaurant_detail", args=[slug]))
 
 
+@login_required
 def comment_delete(request, slug, comment_id):
+
     """
-    view to delete comment
+   Allows a logged-in user to delete their own comment.
     """
+
     queryset = Restaurant.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
@@ -102,6 +110,11 @@ def comment_delete(request, slug, comment_id):
 
 @login_required
 def toggle_favourite(request, slug):
+
+    """
+    Allows a logged-in user to add or remove a restaurant from their favourites.
+    """
+
     restaurant = get_object_or_404(Restaurant, slug=slug)
 
     if request.user in restaurant.favourites.all():
@@ -109,11 +122,16 @@ def toggle_favourite(request, slug):
     else:
         restaurant.favourites.add(request.user)
 
-    return redirect(request.META.get("HTTP_REFERER") or "home")
+    return redirect(request.META.get("HTTP_REFERER", reverse("home")))
 
 
 @login_required
 def favourite_list(request):
+
+    """
+    Displays a list of restaurants favourited by the logged-in user.
+    """
+
     favourites = request.user.favourite.all()
     return render(
         request, "favourite_list.html", {"favourites": favourites})
@@ -121,6 +139,11 @@ def favourite_list(request):
 
 @login_required
 def toggle_like(request, slug):
+
+    """
+    Allows a logged-in user to like or unlike a restaurant.
+    """
+
     restaurant = get_object_or_404(Restaurant, slug=slug)
 
     if request.user in restaurant.likes.all():
