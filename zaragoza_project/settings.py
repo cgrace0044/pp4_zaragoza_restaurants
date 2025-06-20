1bd5879cc895d442820cdb446aebf8dd77b03d0c
cgrace0044 <cgrace0044@gmail.com>
2025-06-20 12:40:57 +0200

    improving lighthouse score by making cloudinary secure and improving heading sequentiality

--- zaragoza_project/settings.py
+++ zaragoza_project/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 
 if os.path.isfile("env.py"):
     import env
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,6 +60,11 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Add Cloudinary config to ensure images secure
+cloudinary.config(
+    secure=True,
+)
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
